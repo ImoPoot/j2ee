03ac2a507f04b2cce9c1192bca54a6a5ef209bd7
ImoPoot <38577623+ImoPoot@users.noreply.github.com>
2020-04-10 16:13:31 +0800

    test for project from idea

--- src/test.java
+++ src/test.java
@@ -1,6 +1,6 @@
 public class test {
     public static void main(String[] args)
     {
-        System.out.println("hello git,it is a project from IDEA");
+        System.out.println("hello git,it is a project from IDEA,a little change");
     }
 }
